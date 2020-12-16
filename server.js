bef7c0e1f62e6ea61ea19f3bd00ba18c8a0e1fe9
Lisa Sturegard <lisasturegard@hotmail.com>
2020-12-16 11:39:16 +0100

    Started project, created basic user

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 3
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/shopping'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.Promies = Promise
 
 const port = process.env.PORT || 8080
 const app = express()
@@ -8,8 +33,47 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavaiable' })
+  }
+})
+
+const authenticateUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ accessToken: req.header('Authorization') })
+    if (user) {
+      req.user = user
+      next()
+    } else {
+      res.status(401).json({ loggedOut: true })
+    }
+  } catch (err) {
+    res.stauts(403).json({ message: 'Access token missing or wrong' })
+  }
+}
+
 app.get('/', (req, res) =>
-  res.send('Hello World')
+  res.send('Shopping List')
 )
+
+app.post('/users', async (req, res) => {
+  try {
+    const { name, password } = req.body
+    const rounds = 5
+    const user = new User({ name: name, password: bcrypt.hashSync(password, rounds) })
+    await user.save()
+    res.status(201).json({ userId: user._id, accessToken: user.accessToken })
+  } catch (err) {
+    res.status(400).json({ message: 'Could not create user', errors: err.errors })
+  }
+})
+
+app.get('/testingauths', authenticateUser)
+app.get('/testingauths', (req, res) => {
+  res.json({ test: 'Testing Authorisation' })
+})
 
 app.listen(port, () => console.log(`Server running on http://localhost:${port}`))
